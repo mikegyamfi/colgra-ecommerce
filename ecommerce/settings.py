1b8da3dbf2b3608ae0d0d3d7d553be0d7ba9b699
Michael <codeage20@gmail.com>
2022-11-14 17:24:57 +0000

    changes to settings - database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'host': os.environ.get("DATABASE_HOST"),
         'port': os.environ.get("DATABASE_PORT"),
-        'NAME': 'colgradb',
+        'NAME': 'seal-app',
         'username' : os.environ.get("DATABASE_USERNAME"),
         'password' :os.environ.get("DATABASE_PASSWORD"),
         'database' : 'colgradb',
