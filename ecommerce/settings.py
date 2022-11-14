fb75047f73c738cd3b813e689214eee529fcaa35
Michael <codeage20@gmail.com>
2022-11-14 18:45:08 +0000

    changes to settings - database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'dmcaprivacy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'host': '',
+        'host': '/var/run/postgresql',
         'port': os.environ.get("DATABASE_PORT"),
         'NAME': 'colgradb',
         'username' : os.environ.get("DATABASE_USERNAME"),
